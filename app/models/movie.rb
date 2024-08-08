33c8013808a79d9fb071db4a5a41f45c7c5cc027
Gabrielle Rochefort <gabrochefort@gmail.com>
2024-08-08 11:50:51 +0200

    completed association and validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,15 @@ class Movie < ApplicationRecord
   #validations
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+
+  before_destroy :ensure_no_bookmarks
+end
+
+private
+
+def ensure_no_bookmarks
+  if bookmarks.exists?
+    errors.add(:base, "Cannot delete a movie with bookmarks")
+    throw(:abort)
+  end
 end
